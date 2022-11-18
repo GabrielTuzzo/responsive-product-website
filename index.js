a5ad3175f9f8ca9cb4b7be0117f41331568a2b10
Gabriel Tuzzo <76889705+GabrielTuzzo@users.noreply.github.com>
2022-11-18 15:30:15 +0000

    new commit

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ menuDrop.addEventListener('click', () => {
     menuDrop.classList.toggle("active-x")
 })
 
+/*---------------------------------------------------------------------
+    File Name: custom.js
+---------------------------------------------------------------------*/
+
+addEventListener("load", () => {
+    const loader =  document.querySelector(".loader")
+    
+    loader.classList.add("loader-hidden")
+
+    document.querySelector(".loader").addEventListener("transitionend", () => {
+        document.body.removeChild(document.querySelector(".loader"))
+    })
+})
